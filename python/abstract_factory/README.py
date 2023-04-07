195a1a62693dfcfe8d1878b52eb15a35d5b3d938
zorgulle <winy95battle@gmail.com>
2023-04-07 12:31:48 +0200

    Add Readme

---
+++ python/abstract_factory/README.py
@@ -0,0 +1,6 @@
+# Roles:
+* Concrete objects: Object that implements all methods of an abstract object
+* Abstract objects: Define all method that could be use by the client and must be implemented by the subclass
+* Concrete factory: implement interface for operation that create the concrete object
+* Abstract factory: Define interface for operation that create abstract object
+* Client: User of the abstract factory and abstract product. The client only knows the intercace. Decoupled from the concrete implementation
